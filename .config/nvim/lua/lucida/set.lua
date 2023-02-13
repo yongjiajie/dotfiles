c8c7a05c05fc1725e2104fe2a66cc5a875428d8e
Yong Jia Jie <yjjyongjiajie@gmail.com>
2023-02-13 18:52:26 +0800

    refactor: add comments to set.lua

--- .config/nvim/lua/lucida/set.lua
+++ .config/nvim/lua/lucida/set.lua
@@ -1,27 +1,40 @@
+-- File numbering
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Spaces
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2 
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+-- Wrap
 vim.opt.wrap = false
 
+-- Undo
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- Search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- Colors
 vim.opt.termguicolors = true
 
+-- Scrolling
 vim.opt.scrolloff = 8
 
+-- Updates
 vim.opt.updatetime = 50
 
+-- Guiding Column
 vim.opt.colorcolumn = "80"
 
+-- Leader
 vim.g.mapleader = " "
+
+-- Cursor
+vim.opt.guicursor = ""
